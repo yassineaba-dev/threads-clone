5ddb1b662be886b16fbfce9d7dc6368f40e39ee9
yassineaba-dev <yassineaba.dev@gmail.com>
2025-07-22 16:50:42 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd());
-	const API_URL = env.VITE_API_URL;
-
-	return {
-		plugins: [react()],
-		server: {
-			port: 5173,
-			proxy: {
-				"/api": {
-					target: API_URL,
-					changeOrigin: true,
-					secure: false,
-					configure: (proxy) => {
-						proxy.on("proxyReq", (proxyReq, req) => {
-							proxyReq.setHeader("Origin", API_URL);
-						});
-					},
-				},
-			},
-		},
-	};
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
