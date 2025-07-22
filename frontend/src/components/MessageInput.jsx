b25c1be1ee0ea4819d32390f335f5ddce61ad351
yassineaba-dev <yassineaba.dev@gmail.com>
2025-07-22 16:59:53 +0100

    Update MessageInput.jsx

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -39,7 +39,7 @@ const MessageInput = ({ setMessages }) => {
 		setIsSending(true);
 
 		try {
-			const res = await fetch("/api/app2/messages", {
+			const res = await fetch(API_URL + "/api/app2/messages", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
